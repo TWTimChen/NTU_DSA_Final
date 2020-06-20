99b9ff4997517e30e4f0866019f1a6e8ce57c077
onionlai <chaojunglai21@gmail.com>
2020-06-21 00:25:29 +0800

    還是找不出bug 目前query find和預期不同

--- mailDB.cpp
+++ mailDB.cpp
@@ -567,3 +567,31 @@ MailDB::print_candidate(){
     }
     cout<<endl;
 }
+
+int main()
+{
+    string inputLine;
+    vector<string> lineSplit;
+
+    MailDB mailDB;
+
+    while(getline(cin, inputLine)){
+        if(inputLine.size()==0) break;
+        split(inputLine, lineSplit);
+        if (lineSplit[0]=="add") {
+            mailDB.add(lineSplit[1]);
+        }
+        else if (lineSplit[0]=="remove") {
+            unsigned id = stoi(lineSplit[1]);
+            mailDB.remove(id);
+        }
+        else if (lineSplit[0]=="longest") {
+            mailDB.longest();
+        }
+        else if (lineSplit[0]=="query") {
+            vector<string> args(lineSplit.begin()+1, lineSplit.end());
+            mailDB.query(args, LESS);
+        }
+        inputLine.clear();
+    }
+}
